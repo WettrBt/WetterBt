ed4a5bf1c7308fb0985e39c2cb0cd1cc7568e854
Richarde Richarde Richardsson <reresson@gmail.com>
2018-08-01 14:47:44 +0200

    version 1.01.2

--- app.py
+++ app.py
@@ -159,7 +159,4 @@ def get_weather_data(req):
 
 
 if __name__ == "main":
-    port = int(os.getenv("PORT", 5000))
-
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host="0.0.0.0")
+        app.run(debug=False, port=port, host="0.0.0.0")
